a161d7390fea714c46d77d3f290e625c381e0e8a
thelittlecitizen13 <thelittlecitizen13@gmail.com>
2020-08-31 09:12:04 +0300

    Solution added

---
+++ House_Of_Cards/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace House_Of_Cards
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
