3cb7d33ccf6af2c2c57ecc5fb8292221b1c4b722
thelittlecitizen13 <thelittlecitizen13@gmail.com>
2020-08-31 09:19:41 +0300

    Card class implemented

---
+++ House_Of_Cards/Card.cs
@@ -0,0 +1,32 @@
+﻿namespace House_Of_Cards
+{
+    public class Card
+    {
+        public int Number { get; set; }
+        public string Color { get; set; }
+        public bool IsExposed { get; set; }
+        public Card(int num, string color)
+        {
+            Number = num;
+            Color = color;
+            IsExposed = false;
+        }
+        public string ExposeCard()
+        {
+            IsExposed = true;
+            return ShowCard();
+        }
+        public string ShowCard()
+        {
+            if (IsExposed)
+                return $"Color: {Color}, Number: {Number}";
+            else
+                return "Card didnt expose";
+        }
+        public string ShowToGroupBuddy()
+        {
+            return $"Color: {Color}, Number: {Number}";
+        }
+
+    }
+}
